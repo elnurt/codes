48033f9f632fd2407a77f9aedd0fa073e8f4d74a
elnurt <35156215+elnurt@users.noreply.github.com>
2023-01-23 14:25:36 +0400

    Create ad_repl.ps1

---
+++ ad_repl.ps1
@@ -0,0 +1,25 @@
+﻿# Function to get replication status 
+function Get-ReplicationStatus {
+    $replicationStatus = repadmin /replsummary 
+    return $replicationStatus
+}
+
+# Function to get the errors and warnings 
+function Get-ReplicationErrors {
+    $replicationErrors = dcdiag /test:replications
+    return $replicationErrors
+}
+
+# Calling the function and storing the result in a variable
+$status = Get-ReplicationStatus
+$errors = Get-ReplicationErrors
+
+# Checking if there are any replication errors and printing the result
+if ($errors -match "failed" -or $errors -match "warning") {
+    Write-Host "Replication status: Error"
+    Write-Host $errors
+}
+else {
+    Write-Host "Replication status: Success"
+    Write-Host $status
+}
