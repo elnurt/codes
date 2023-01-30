0505c83dee4f293e3c58c1565d5d33f74b3d0cd8
elnurt <35156215+elnurt@users.noreply.github.com>
2023-01-30 22:42:48 +0400

    Add files via upload

---
+++ service name by port.ps1
@@ -0,0 +1,4 @@
+﻿$port = Read-Host "Enter a port number"
+$ProcessID = (Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess).Id
+$service = Get-WmiObject Win32_Service -Filter "ProcessId='$ProcessID'"
+$service.DisplayName
