1b6c393fdb329c753d4f78dc9e1b4ec3b7e3b384
elnurt <35156215+elnurt@users.noreply.github.com>
2023-01-31 23:53:58 +0400

    Add files via upload

---
+++ set_static_ip.ps1
@@ -0,0 +1,15 @@
+﻿get-netadapter
+
+
+$index = Read-Host "Enter ifIndex numeric number  of the interface to assign static IP"  
+$IpAdress = Read-Host  "Enter new IP"  
+$DG = Read-Host " Enter Gateway"  
+$subnet = Read-Host "Subnet in a bit format"  
+$DNS = Read-Host " DNS"  
+
+ 
+
+
+
+New-NetIPAddress –IPAddress $IpAdress -DefaultGateway $DG -PrefixLength $subnet -InterfaceIndex $index
+Set-DNSClientServerAddress –InterfaceIndex $index  –ServerAddresses $DNS
